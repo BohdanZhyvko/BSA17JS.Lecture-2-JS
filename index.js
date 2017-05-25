57ae54e831659d24719f5ee102047a81bfb80ffa
BohdanZhyvko <bohdan.zhyvko@gmail.com>
2017-05-25 17:03:23 +0300

    added function 'fight'

--- index.js
+++ index.js
@@ -18,11 +18,31 @@ class Fighter {
 }
 
 class ImprovedFighter extends Fighter {
-  doubleHit(){
+  doubleHit(enemy, point){
     let doublePoint = point*2;
-    super.hit(enemy, doublePoint)
+    super.hit(enemy, doublePoint);
   }
 }
 
-var fighter = new Fighter();
-var improvedFighter = new ImprovedFighter();
+var fighter = new Fighter(first, 25, 500);
+var improvedFighter = new ImprovedFighter(second, 30, 500);
+var point = [25, 13, 45];
+
+var fight = (fighter, improvedFighter, ...point) => {
+  while(fighter.health > 0 || improvedFighter.health > 0){
+    for(let value of point){
+      fighter.hit(improvedFighter, value);
+
+      if(fighter.health > 0){
+      	break;
+      }
+
+      improvedFighter.hit(fighter, value);
+    }
+  }
+  if(fighter.health > 0){
+    console.log(`${fighter.name} was won! His health: ${fighter.health}`);
+  }else{
+    console.log(`${improvedFighter.name} was won! His health: ${improvedFighter.health}`);
+  }
+}
