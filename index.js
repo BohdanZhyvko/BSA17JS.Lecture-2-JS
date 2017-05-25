6b4374080d720f1ca3c21b958721c711c042ae01
BohdanZhyvko <bohdan.zhyvko@gmail.com>
2017-05-25 17:59:28 +0300

    added start point and fixed error

--- index.js
+++ index.js
@@ -24,25 +24,31 @@ class ImprovedFighter extends Fighter {
   }
 }
 
-var fighter = new Fighter(first, 25, 500);
-var improvedFighter = new ImprovedFighter(second, 30, 500);
-var point = [25, 13, 45];
+var fighter = new Fighter('first', 25, 32500);
+var improvedFighter = new ImprovedFighter('second', 30, 3500);
 
 var fight = (fighter, improvedFighter, ...point) => {
   while(fighter.health > 0 || improvedFighter.health > 0){
+    
     for(let value of point){
       fighter.hit(improvedFighter, value);
 
-      if(fighter.health > 0){
+      if(improvedFighter.health <= 0 || fighter.health <= 0){
       	break;
       }
 
       improvedFighter.hit(fighter, value);
     }
+    if(improvedFighter.health <= 0 || fighter.health <= 0){
+      	break;
+      }
   }
   if(fighter.health > 0){
     console.log(`${fighter.name} was won! His health: ${fighter.health}`);
   }else{
     console.log(`${improvedFighter.name} was won! His health: ${improvedFighter.health}`);
   }
-}
+};
+
+//start
+fight(fighter, improvedFighter, point);
